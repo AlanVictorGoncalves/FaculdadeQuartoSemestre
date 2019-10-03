5265e6fc85b933892f7e73be84db16d91ad83af1
alanvictorgoncalves <Ken00Facebookisom@hotmail.com>
2019-10-03 16:00:02 -0300

    Mais correções, aprimoramentos, colocar CD Pillon

--- Construção de Websites/Projetos Java/Feijao/Feijao.java
+++
@@ -1,9 +0,0 @@
-/**
- * Feijao
- */
-public class Feijao {
-
-    public static void main(String[] args) {
-        System.out.println("Feijão!");
-    }
-}
--- Construção de Websites/Projetos Java/HelloWorld/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("Hello, World. POTATO");
-        System.out.println("");
-    }
-}
